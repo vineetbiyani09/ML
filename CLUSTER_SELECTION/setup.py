4aca9bc6ea45eb470b474509b9c32a6cf7a91172
vineetbiyani09 <67839378+vineetbiyani09@users.noreply.github.com>
2020-10-31 16:03:47 +0530

    Update setup.py

--- CLUSTER_SELECTION/setup.py
+++ CLUSTER_SELECTION/setup.py
@@ -16,7 +16,7 @@ def readme():
         return f.read()
 
 setup(name='CLUSTER_SELECTION',
-      version='0.2.9',
+      version='3.9.1',
       description='Learning setup',
       long_description= readme(),
       url='https://github.com/vineetbiyani09/ML/tree/master/CLUSTER_SELECTION',
